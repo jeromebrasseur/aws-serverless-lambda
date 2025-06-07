ba3231da7de82d2938ad97dbf9e00d94a4d2f151
Jérôme BRASSEUR <raxiocool@gmail.com>
2025-06-07 12:31:52 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,41 @@
 provider "aws" {
-  region = var.aws_region
+  region = var.AWS_REGION
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "Lambda_Function_Role"
+  assume_role_policy = <<E0F
+  {
+    "Version" : "2012-10-07"
+    "Statement" : [
+      {
+        "Action" : "sts:AssumeRole",
+        "Principal" : {
+          "Service" : "lambda.amazonaws.com"
+        },
+        "Effect" : "Allow",
+        "Sid" : ""
+      }
+    ]
+  }
+  E0F
+}
+
+data "archive_file" "lambda_hello_world" {
+  type = "zip"
+
+  source_dir = "${path.module}/app"
+  output_path = "${path.module}/app.zip"
+}
+
+resource "aws_lambda_function" "helloworld_lambda_function" {
+  function_name = var.LAMBDA_FUNCTION_NAME
+  handler = "hello.handler"
+  runtime = "nodejs22.x"
+  role = aws_iam_role.lambda_role.arn
+  filename = "${data.archive_file.lambda_hello_world.output_path}"
+  tags = {
+    Name = var.LAMBDA_FUNCTION_NAME
+    Env = "DEV"
+  }
 }
