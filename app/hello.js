aebcfce691adeb9348b4b2fe0a27bd393343d783
Jérôme BRASSEUR <raxiocool@gmail.com>
2025-06-07 11:39:03 +0200

    Update hello.js

--- app/hello.js
+++ app/hello.js
@@ -1,1 +1,14 @@
+module.exports.handler = async (event) => {
+  console.log('Event: ', event);
+  let responseMessage = 'Hello world !';
 
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: json.stringify({
+      message: responseMessage,
+    }),
+  }
+}
